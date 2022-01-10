6649fff180dd2d8a89db2c5ba422cf69da37ced3
MikiKung <mikiofficial002@gmail.com>
2022-01-11 05:05:40 +0700

    update

--- next.config.js
+++ next.config.js
@@ -2,15 +2,16 @@
 const isProd = process.env.NODE_ENV === "production";
 const path = require('path')
 module.exports = {
-  target: 'serverless',
+  // target: 'serverless',
   exportPathMap: () => ({
     "/": {
       page: "/",
     },
   }),
-  // images: {
-  //   loader: 'imgix',
-  // },
+  images: {
+    loader: 'imgix',
+  },
+  // reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
