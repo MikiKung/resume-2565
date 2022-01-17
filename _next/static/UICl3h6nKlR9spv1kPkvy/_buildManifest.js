546b96f1c9f998fe87d1f85e201031ad8c515f49
MikiKung <87208702+MikiKung@users.noreply.github.com>
2022-01-17 18:02:39 +0000

    Deploying to gh-pages from  @ 320af607820415739a10ed137ad7585f532a9434 🚀

---
+++ _next/static/UICl3h6nKlR9spv1kPkvy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/54578eac2ef98e03.css","static/chunks/pages/index-2bd30d147183644e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
